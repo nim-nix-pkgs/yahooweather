f631388d18984df17b2b14229c58a344696631fa
hugosenari <hugosenari@gmail.com>
2022-03-11 23:27:18 -0300

    chore: re index

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''Yahoo! Weather API wrapper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."yahooweather-master".url = "path:./master";
+  inputs."yahooweather-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."yahooweather-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Yahoo! Weather API wrapper'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-yahooweather-master.flake = false;
+  inputs.src-yahooweather-master.owner = "achesak";
+  inputs.src-yahooweather-master.ref   = "refs/heads/master";
+  inputs.src-yahooweather-master.repo  = "nim-yahooweather";
+  inputs.src-yahooweather-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-yahooweather-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-yahooweather-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
