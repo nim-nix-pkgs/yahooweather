387cf8c6e0e5a4dddf0f1dd39ea996038706c014
hugosenari <hugosenari@gmail.com>
2022-05-12 11:59:07 -0300

    chore: re index yahooweather

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."yahooweather-master".dir   = "master";
+  inputs."yahooweather-master".owner = "nim-nix-pkgs";
+  inputs."yahooweather-master".ref   = "master";
+  inputs."yahooweather-master".repo  = "yahooweather";
+  inputs."yahooweather-master".type  = "github";
+  inputs."yahooweather-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."yahooweather-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
